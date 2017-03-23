5258ced87ee12b79ccc77947b43629df9852ef9e
yulew <yulew@sfu.ca>
2017-03-23 16:51:22 -0700

    Update AddTwoNumbers.py

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -6,6 +6,14 @@
 
 
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         """
@@ -13,30 +21,28 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-    
-        l3=[0]
-        digit=l1.val+l2.val
-        l3[0]=(digit)%10
-        while l1.next is not None or l2.next is not None:
+        digit=0
+        l3=[]
+        while l1 is not None or l2 is not None:
             try:
                 if digit>=10:
-                    digit=l1.next.val+l2.next.val+1
+                    digit=l1.val+l2.val+1
                 else:
-                    digit=l1.next.val+l2.next.val
+                    digit=l1.val+l2.val
                 l1=l1.next
                 l2=l2.next
             except:  #If l1.next or l2.next is NoneType
                 try:
                     if digit>=10:
-                        digit=l1.next.val+1
+                        digit=l1.val+1
                     else:
-                        digit=l1.next.val
+                        digit=l1.val
                     l1=l1.next
                 except:
                     if digit>=10:
-                        digit=l2.next.val+1
+                        digit=l2.val+1
                     else:
-                        digit=l2.next.val
+                        digit=l2.val
                     l2=l2.next
             l3.append(digit%10)
         if digit>=10:
